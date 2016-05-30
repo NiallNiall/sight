0e36f2d8800617d5baa9e085d8b3b0b5769dd463
NiallNiall <niall.henn@gmail.com>
2016-05-30 16:23:42 +0100

    twig

--- src/js/twig.js
+++ src/js/twig.js
@@ -19,15 +19,15 @@ paper.install(window);
     // Create an empty project and a view for the canvas:
     paper.setup(canvas);
     // Create a Paper.js Path to draw a line into it:
-    // var myCircle = new paper.Path.Circle(new paper.Point(100, 70), 50);
-    // myCircle.fillColor = 'black';
+    var testCircle = new paper.Path.Circle(new paper.Point(100, 70), 5);
+    testCircle.fillColor = 'black';
 
     var Item = new Symbol();
     var leaf = new Symbol();
 
     project.importSVG('/img/leaf.svg', function(item) {
                       // console.log(item);
-                      item.scale(0.15);
+                      item.scale(0.15, -0.15);
                       // item.rotate(140);
                       Item = new Symbol(item);
                       leaf = Item.place(new paper.Point(100,100));
@@ -41,17 +41,27 @@ paper.install(window);
 
     paper.view.onFrame = function(event) {
 
-      if(testRot<360){
+      var animTime = 100;
+      var animAmt = 120;
+
+      if(testRot<animTime){
         testRot += 1;
 
       } else {
         testRot = 0;
       }
-              // testRot = easeInOutExpo(testRot, 0, 360, 360);
-              leaf.rotation = easeInOutExpo(testRot, 0, 360, 360);
-      // console.log(leaf);
-      // leaf.rotation = 45;// = paper.view.center;
+      var pivotPoint = new paper.Point(-28,0);
+      leaf.pivot = pivotPoint;
+      testCircle.position = leaf.position;
+      // testRot = easeInOutExpo(testRot, 0, 360, 360);
+      leaf.rotation = easeOutExpo(testRot, 0, animAmt, animTime);
+    }
 
+
+    var mouseTool = new paper.Tool();
+
+    mouseTool.onMouseDown = function(event) {
+      leaf = Item.place(event.point);
     }
 
 
@@ -62,20 +72,10 @@ paper.install(window);
 
 
 
-  // t: current time, b: begInnIng value, c: change In value, d: duration
-function easeInQuad(t, b, c, d) {
-    return c*(t/=d)*t + b;
-}
 
-function easeInOutExpo(t, b, c, d) {
-    if (t==0) return b;
-    if (t==d) return b+c;
-    if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-    return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-  }
-function easeInExpo(t, b, c, d) {
-    return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
-  }
-function  easeOutExpo(t, b, c, d) {
-    return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
-  }
+
+
+
+
+
+  
